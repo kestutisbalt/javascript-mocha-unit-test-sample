dd052fa4bb2297a603c8f57adbeaaab91329668d
Kestutis Baltronas <kestutisbalt@gmail.com>
2015-04-14 15:10:38 +0300

    Added test to validate arguments passed to add function.

--- test/calculator-test.js
+++ test/calculator-test.js
@@ -4,6 +4,15 @@ describe('Calculator', function() {
 	 * Setup method. Initializes environment for each test.
 	 */
 	before(function() {
+		this.aCalculator = new Calculator();
+	});
+
+	describe('Adding', function() {
+
+		it("should throw an error if a non-numeric value is used", function() {
+			expect(this.calculator.add.bind(this.aCalculator, 2, 'a')).to.throw();
+		});
+
 	});
 
 });
