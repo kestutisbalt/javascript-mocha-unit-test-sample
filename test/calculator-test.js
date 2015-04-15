853179993207c67a80f788ae254a464c34fc8e6f
Kestutis Baltronas <kestutisbalt@gmail.com>
2015-04-15 07:36:53 +0300

    Imported asserion module chai and calculator.

--- test/calculator-test.js
+++ test/calculator-test.js
@@ -1,16 +1,21 @@
+var expect = require('chai').expect;
+
+var Calculator = require('../src/calculator.js');
+
+
 describe('Calculator', function() {
 
 	/**
 	 * Setup method. Initializes environment for each test.
 	 */
 	before(function() {
-		this.aCalculator = new Calculator();
+		this.aCalculator = new Calculator.Calculator();
 	});
 
 	describe('Adding', function() {
 
 		it("should throw an error if a non-numeric value is used", function() {
-			expect(this.calculator.add.bind(this.aCalculator, 2, 'a')).to.throw();
+			expect(this.aCalculator.add.bind(this.aCalculator, 2, 'a')).to.throw();
 		});
 
 	});
